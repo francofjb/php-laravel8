735ac49247be8579066db10f0be3a9412383ec95
francofjb <francofjb@gmail.com>
2021-05-06 13:58:14 -0300

    prueba git

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'Show-Mostrar' . $curso)
 @section('content')
-    <h1>METODO SHOW - Bienvenido al curso: <?php echo $curso; ?></h1>
+    <h1>METODO SHOW git - Bienvenido al curso: <?php echo $curso; ?></h1>
     
     {{--  otra forma de mostrar la variable   --}}
     <h1>METODO SHOW - Bienvenido al curso: {{$curso}} - otra forma de mostrar la variable en blade</h1> 

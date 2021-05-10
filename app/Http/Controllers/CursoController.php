b43204e3c6dbfca70b6b3e0d5e3012d56944808d
francofjb <francofjb@gmail.com>
2021-05-10 20:50:32 -0300

    curso13

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Curso;
 use Illuminate\Http\Request;
 
 class CursoController extends Controller
 {
     public function index(){
         
-        //return "METODOINDEX - Bienvenidos al curso Franco-Laravel";
-        return view('cursos.index');
+        //La variable $cursos guarda todos los registros recuperados en forma de coleccion PAGINADA
+        $cursos = Curso::paginate();
+        
+        //Ahora, vamos a pasar le variable $cursos a la vista con compact()
+        return view('cursos.index', compact('cursos'));
     }
 
     public function create(){
--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -3,4 +3,12 @@
 @section('title', 'Cursos')
 @section('content')
     <h1>METODO INDEX - Bienvenidos al curso Franco-Laravel</h1>
+    <ul>
+        @foreach($cursos as $curso)
+            <li>{{$curso->name}}</li>
+        @endforeach
+    </ul>   
+    
+        {{$cursos->links()}}
+        
 @endsection    

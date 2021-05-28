2c51772f61590147feb9003f76f00e14a80c155a
francofjb <francofjb@gmail.com>
2021-05-28 14:31:47 -0300

    video23

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ContactanosController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CursoController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,7 +20,10 @@ use App\Http\Controllers\CursoController;
 Route::get('/', HomeController::class)->name('home');
 
 Route::resource('cursos', CursoController::class);
-
+    
 Route::view('nosotros', 'nosotros')->name('nosotros');
 
+Route::get('contactanos', [ContactanosController::class, 'index'])->name('contactanos.index');
+
+Route::post('contactanos', [ContactanosController::class, 'store'])->name('contactanos.store');
 
